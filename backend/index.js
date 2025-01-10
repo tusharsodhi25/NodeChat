4a6d4dcf08feff9b8d7736246b2721bf878751fb
Tushar Sodhi <116172693+tusharsodhi25@users.noreply.github.com>
2025-01-10 16:05:58 +0530

    Update index.js

--- backend/index.js
+++ backend/index.js
@@ -17,6 +17,14 @@ app.use(express.json({ limit: '50mb' })); // You can adjust this value
 app.use(cookieParser());
 
 
+app.use(express.static(path.join(__dirname, 'build')));
+
+
+app.get('*', (req, res) => {
+  res.sendFile(path.join(__dirname, 'build', 'index.html'));
+});
+
+
 
 
 const fileUpload = require('express-fileupload');
