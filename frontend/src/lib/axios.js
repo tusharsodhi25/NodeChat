728dc67f4e727e37f94f6a3e6e7aef4447c9c887
Tushar Sodhi <116172693+tusharsodhi25@users.noreply.github.com>
2025-01-10 15:58:32 +0530

    Update axios.js

--- frontend/src/lib/axios.js
+++ frontend/src/lib/axios.js
@@ -1,7 +1,8 @@
 import axios from "axios";
 
 export const axiosInstance = axios.create({
-  baseURL:"http://localhost:3001/api",
+  baseURL:"https://nodechat-mo4m.onrender.com",
+   // baseURL:"http://localhost:3001/api",
   withCredentials: true,
 
 });
